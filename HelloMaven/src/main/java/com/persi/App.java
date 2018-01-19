6b677b989f94400eaddd7d4378988df6b7c3279d
Shwetali <shwetali_berad@yahoo.com>
2018-01-19 15:36:12 +0530

    Adding test maven project WS

---
+++ HelloMaven/src/main/java/com/persi/App.java
@@ -0,0 +1,13 @@
+package com.persi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
